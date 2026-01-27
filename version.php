1b70fee87767bb166b0b2abb55bb52fa607a6ca1
aleko21 <36449858+aleko21@users.noreply.github.com>
2026-01-27 22:29:15 +0100

    Update version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_mmonitor'; // Nome univoco del plugins
-$plugin->version   = 2026012703;
+$plugin->version   = 2026012705;
 $plugin->requires  = 2023100900;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0';
