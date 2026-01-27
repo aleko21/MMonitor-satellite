2d774a92dbdac765ee96e740254c014fd795cfd1
aleko21 <36449858+aleko21@users.noreply.github.com>
2026-01-27 22:33:16 +0100

    Revise security key description and add log retention options
    Updated descriptions for security and log retention settings.

--- lang/en/local_mmonitor.php
+++ lang/en/local_mmonitor.php
@@ -1,15 +1,15 @@
 <?php
 $string['pluginname'] = 'MMonitor Satellite';
+$string['dashboard'] = 'MMonitor Dashboard';
+$string['go_to_dashboard'] = 'Vai alla Dashboard Visuale';
 $string['vps_ip'] = 'VPS IP Address';
 $string['vps_ip_desc'] = 'Only requests from this IP will be allowed to access JSON files.';
 $string['secret_key'] = 'Secret Security Key';
-$string['secret_key_desc'] = 'This key is part of the filename for obfuscation.';
+$string['secret_key_desc'] = 'This key is part of the filename for security.';
 $string['log_retention'] = 'Log Retention';
+$string['log_retention_desc'] = 'How many days of history to keep.';
+$string['7days'] = '7 Days';
+$string['14days'] = '14 Days';
+$string['30days'] = '30 Days';
 $string['task_name'] = 'MMonitor: Generate Status Report';
-$string['go_to_dashboard'] = 'Vai alla Dashboard Visuale';
-$string['vps_ip_desc'] = 'Inserisci l\'IP della tua VPS. Solo questo IP potrà scaricare i file JSON via web.';
-$string['secret_key_desc'] = 'Questa chiave verrà inclusa nei nomi dei file JSON generati per renderli sicuri.';
-$string['log_retention_desc'] = 'Quanti giorni di file storici vuoi conservare nella cartella monitor_data?';
-$string['7days'] = '7 giorni';
-$string['14days'] = '14 giorni';
-$string['30days'] = '30 giorni';
+$string['dashboard_heading'] = 'Moodle Satellite Status';
